3a05451f6aa2f9f16d573050ba3c6a177a9b2dc8
仇灾幼 <26304262+QIUZAIYOU@users.noreply.github.com>
2024-03-06 23:14:34 +0800

    Update PostPreview_reborn.js

--- PostPreview_reborn.js
+++ PostPreview_reborn.js
@@ -15,16 +15,17 @@
 (function () {
     'use strict';
     const selecter = {
-        tap: '#tap',
+        tap: '#__nuxt',
         momentCardFooter: '.moment-card__footer',
         previewWrapper: '#previewWrapper',
         previewIframe: '#previewIframe',
         previewIframeMask: '#previewIframeMask',
         previewContentHeader: 'header',
-        previewContentMain: 'main'
+        previewContentMain: 'main',
+        previewButton: '.previewButton',
     }
     const styles = {
-        PreviewWrapperStyle: '#previewWrapper{position:fixed;top:50%;left:50%;overflow:hidden;box-sizing:border-box;width:600px;height:97vh;border:2px solid #00d9c5;border-radius:10px;background:#191919;transform:translate(-50%,-50%)}#previewWrapper::backdrop{backdrop-filter:blur(3px)}#previewIframe{width:100%;height:100%;border:none;background:#191919}#previewIframeMask{position:absolute;display:flex;background:#191919;inset:0;align-items:center;justify-content:center}',
+        PreviewWrapperStyle: '#previewWrapper{position:fixed;top:50%;left:50%;overflow:hidden;box-sizing:border-box;width:600px;height:97vh;border:2px solid #00d9c5;border-radius:10px;background:#191919;transform:translate(-50%,-50%)}#previewWrapper::backdrop{backdrop-filter:blur(3px)}#previewIframe{width:100%;height:100%;border:none;background:#191919}#previewIframeMask{position:absolute;display:flex;background:#191919;inset:0;align-items:center;justify-content:center}.previewButton{cursor: pointer}',
         PreviewIframeStyle: 'body{background:#191919!important}header{display:none!important}main{margin-left:0!important}'
     }
     const utils = {
@@ -45,7 +46,7 @@
          */
         createElementAndInsert(HtmlString, target, method) {
             const element = elmGetter.create(HtmlString, target)
-            target[method](element)
+            target[ method ](element)
             return element
         },
         /**
@@ -87,14 +88,14 @@
         },
         async insertPreviewButtonToMomentCard() {
             const previewButtonHtml = `
-            <span class="icon-button flex-center--y moment-card_repost--btn moment-card__footer-btn" data-booth-item="" data-track-prevent="click" data-booth-level="1">
+            <span class="previewButton icon-button flex-center--y moment-card_repost--btn moment-card__footer-btn" data-booth-item="" data-track-prevent="click" data-booth-level="1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" class="icon" viewBox="0 0 1395 1024">
                     <path fill="#98999e" d="M698.098 1023.954c-249.238 0-477.245-153.882-680.697-455.598a106.157 106.157 0 0 1 0-115.804C220.853 153.882 448.767 0 698.098 0s477.245 153.882 680.697 455.598a106.157 106.157 0 0 1 0 115.804c-203.637 298.67-431.459 452.552-680.697 452.552zm0-920.382c-212.637 0-412.997 138.466-596.556 409.905C286.624 784.731 487.03 923.382 698.098 923.382s412.997-138.466 596.556-409.905C1110.91 240.746 910.412 103.618 698.098 103.618z" />
                     <path fill="#98999e" d="M697.913 717.714a204.19 204.19 0 1 1 204.976-204.19 204.606 204.606 0 0 1-204.976 204.19zm0-307.81A102.095 102.095 0 1 0 800.424 512a102.28 102.28 0 0 0-102.51-102.095z" />
                 </svg>
             </span>
             `
-            const [$previewWrapper, $previewIframe, $previewIframeMask] = await elmGetter.get([selecter.previewWrapper, selecter.previewIframe, selecter.previewIframeMask])
+            const [ $previewWrapper, $previewIframe, $previewIframeMask ] = await elmGetter.get([ selecter.previewWrapper, selecter.previewIframe, selecter.previewIframeMask ])
             const previewIframeWindow = $previewIframe.contentWindow
             $previewWrapper.addEventListener('toggle', (event) => {
                 if (event.newState === 'closed') {
@@ -104,14 +105,15 @@
                 }
             })
             $previewIframe.addEventListener('load', async () => {
-                const [$previewContentHeader, $previewContentMain] = await elmGetter.get([selecter.previewContentHeader, selecter.previewContentMain], previewIframeWindow.document)
+                const [ $previewContentHeader, $previewContentMain ] = await elmGetter.get([ selecter.previewContentHeader, selecter.previewContentMain ], previewIframeWindow.document)
                 if ($previewContentHeader && $previewContentMain) {
                     $previewContentHeader.style.display = 'none'
                     $previewContentMain.style.marginLeft = '0'
                     $previewIframeMask.style.display = 'none'
                 }
             })
-            elmGetter.each(selecter.momentCardFooter, (momentCardFooter) => {
+            await elmGetter.each(selecter.momentCardFooter, (momentCardFooter) => {
+                if (document.querySelector(selecter.momentCardFooter).querySelector(selecter.previewButton)) document.querySelector(selecter.momentCardFooter).querySelector(selecter.previewButton).remove()
                 const previewButton = utils.createElementAndInsert(previewButtonHtml, momentCardFooter, 'append')
                 previewButton.addEventListener('click', (event) => {
                     event.preventDefault()
